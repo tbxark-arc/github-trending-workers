bec84a59c39b54cae258a718b18e98f90523ae95
tbxark <tbxark@outlook.com>
2024-01-24 10:19:18 +0800

    chore: 代码格式化

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import {Router} from 'itty-router';
 /**
  * Handles the fetch requests for the API.
  *
- * @param {Request} req - The request object.
+ * @param {Request} request - The request object.
  * @param {Object} env - The environment object.
  * @param {Object} ctx - The context object.
  * @return {Promise<Response>} - The response object.
@@ -14,24 +14,24 @@ async function fetchHandler(request, env, ctx) {
   // eslint-disable-next-line
   const router = Router();
   const josnResponse = (obj, status) => {
-    return new Response(JSON.stringify(obj, null, "  "), {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': '*',
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Expose-Headers': '*',
-          'Content-Type': 'application/json; charset=utf-8',
-        },
-        status: status || 200,
-      });
-  }
+    return new Response(JSON.stringify(obj, null, '  '), {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Expose-Headers': '*',
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      status: status || 200,
+    });
+  };
 
-  router.get('/', () => {
+  router.get('/', (req) => {
     return josnResponse({
-      repositories: `${request.url}repositories`,
-      developers: `${request.url}developers`,
+      repositories: `${req.url}repositories`,
+      developers: `${req.url}developers`,
     }, 200);
-  })
+  });
 
   router.get('/repositories', async (req) => {
     const {language, since, spokenLanguage} = req.query;
@@ -54,7 +54,7 @@ async function fetchHandler(request, env, ctx) {
 
   router.all('*', (req) => {
     return josnResponse({
-      message: 'Not found',
+      message: `Not found: ${new URL(req.url).pathname}`,
     }, 404);
   });
 
